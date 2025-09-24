828a7325de1b03b986dde3d2fce1f2f48ca7469c
Muhammad Sihabuddin Arroyan <muhammad.sihabuddin259@smk.belajar.id>
2025-09-24 14:23:53 +0700

    1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\LocaleMiddleware;
+use App\Http\Middleware\CorsMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
