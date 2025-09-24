7f2b732758afec06bfcb03dc5a1c02e96a1d8948
Muhammad Sihabuddin Arroyan <muhammad.sihabuddin259@smk.belajar.id>
2025-09-24 11:09:57 +0700

    str

--- resources/views/content/document/show.blade.php
+++ resources/views/content/document/show.blade.php
@@ -7,8 +7,7 @@
             <div class="col-md-8">
                 <div class="mb-3">
                     @php
-                        $filePath = Storage::url($document->pdf_file);
- // Path file dokumen
+                        $filePath = asset('storage/' . $document->pdf_file); // Path file dokumen
                         $extension = \Illuminate\Support\Str::lower(pathinfo($document->pdf_file, PATHINFO_EXTENSION));
                         $isPdf = $extension === 'pdf';
                         $isOffice = in_array($extension, ['docx', 'xlsx', 'pptx']);
